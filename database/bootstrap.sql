9a762ab1375ecf90f02609a5c2a7b6cc5e2f9d0a
Bryan Morgan <bryanmorgan@gmail.com>
2021-09-07 10:03:55 -0700

    Simplify bootstrap SQL

--- database/bootstrap.sql
+++ database/bootstrap.sql
@@ -1,13 +1,9 @@
 -- PostgreSQL bootstrap DDL to create a database and a development user account with full access
-CREATE DATABASE timetracker
-WITH
-ENCODING = 'UTF8'
-LC_COLLATE = 'en_US.UTF-8'
-LC_CTYPE = 'en_US.UTF-8'
-TABLESPACE = pg_default
-CONNECTION LIMIT = -1;
+CREATE DATABASE timetracker;
+CREATE USER timetraveler WITH PASSWORD  'timetraveler_changeme';
 
-CREATE ROLE timetraveler WITH LOGIN PASSWORD 'timetraveler_changeme';
+-- Then run: "psql timetracker < schema1.sql"
 
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO timetraveler WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO timetraveler WITH GRANT OPTION;
+-- Then run: "psql timetracker" with the following:
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO timetraveler;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO timetraveler;
